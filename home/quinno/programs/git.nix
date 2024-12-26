5a93f57917219310090c38281ac53fb618c1a96a
Neochaotics <neochaotics@pm.me>
2024-12-25 22:27:03 -0500

    git conf

--- home/quinno/programs/git.nix
+++ home/quinno/programs/git.nix
@@ -8,5 +8,29 @@
 
     userName = "Neochaotics";
     userEmail = "neochaotics@pm.me";
+
+    extraConfig = {
+      core = {
+        editor = nvim;
+      };
+      color = {
+        branch = auto;
+        diff = auto;
+        interactive = auto;
+        pager = true;
+        status = auto;
+        branch = auto;
+        ui = true;
+      };
+      rerere = {
+        enabled = true;
+        autoupdate = true;
+      };
+      rebase = {
+        autoSquash = true;
+      };
+      push.default = upstream;
+      pull.rebase = true;
+    };
   };
 }
