7acee2bd6561b12f6ae7b5278df67792f2ac87e2
Neochaotics <neochaotics@pm.me>
2024-12-27 18:53:24 -0500

    perhaps

--- hosts/libvirt/default.nix
+++ hosts/libvirt/default.nix
@@ -25,7 +25,7 @@
       # When using plymouth, initrd can expand by a lot each time, so limit how many we keep around
       configurationLimit = lib.mkDefault 10;
     };
-    efi.canTouchEfiVariables = true;
+    #efi.canTouchEfiVariables = true;
     timeout = 3;
   };
 
