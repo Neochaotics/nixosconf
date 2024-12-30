2d163fe24277e348c6bd85801fb4b27ed8bfc3be
Neochaotics <neochaotics@pm.me>
2024-12-30 18:28:26 -0500

    hmmm

--- common/hyprland.nix
+++ common/hyprland.nix
@@ -1,8 +1,17 @@
 { inputs
 , pkgs
 , ...
-}:{
-  programs.uwsm.enable = true;
+}: {
+  programs.uwsm = {
+    enable = true;
+    waylandCompositors = {
+      hyprland = {
+        prettyName = "Hyprland";
+        comment = "Hyprland compositor managed by UWSM";
+        binPath = "/run/current-system/sw/bin/Hyprland";
+      };
+    };
+  };
   programs.hyprland = {
     enable = true;
     withUWSM = true; # recommended for most users
