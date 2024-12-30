d68c3f7c52cc5e54ba168ab09132d1021e3a3499
Neochaotics <neochaotics@pm.me>
2024-12-30 08:46:34 -0500

    maybe fix

--- common/sudo.nix
+++ common/sudo.nix
@@ -22,10 +22,6 @@
       ];
       groups = [ "wheel" ];
     }];
-      # Cmnd_Alias:DEBUGGERS = /usr/bin/gdb, /usr/bin/lldb, /usr/bin/strace, /usr/bin/truss, /usr/bin/bpftrace, /usr/bin/dtrace, /usr/bin/dtruss
-      # Cmnd_Alias:PKGMAN = /usr/bin/apt, /usr/bin/dpkg, /usr/bin/rpm, /usr/bin/yum, /usr/bin/dnf,  /usr/bin/zypper, /usr/bin/pacman
-      # Cmnd_Alias:PROCESSES = /usr/bin/nice, /bin/kill, /usr/bin/renice, /usr/bin/pkill, /usr/bin/top
-      # Cmnd_Alias:REBOOT = /sbin/halt, /sbin/reboot, /sbin/poweroff
     extraConfig = with pkgs; ''
       Defaults:always_set_home
       Defaults:requiretty
