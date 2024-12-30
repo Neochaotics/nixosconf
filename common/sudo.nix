9e245569a844f16607a5880a5ca6bf2d2e78f958
Neochaotics <neochaotics@pm.me>
2024-12-30 09:03:24 -0500

    no mor

--- common/sudo.nix
+++ common/sudo.nix
@@ -25,7 +25,6 @@
     extraConfig = with pkgs; ''
       Defaults always_set_home
       Defaults secure_path="${lib.makeBinPath [systemd]}:/nix/var/nix/profiles/default/bin:/run/current-system/sw/bin"
-      Defaults!${lib.makeBinPath [visudo]} env_keep += "SUDO_EDITOR EDITOR VISUAL"
     '';
   };
 }
