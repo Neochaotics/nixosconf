0cb1ddb24c1b473f835adb860b045ef6818f5800
Neochaotics <neochaotics@pm.me>
2025-01-01 14:23:59 -0500

    missing wayland.windowManager.hyprland.settings

--- home/quinno/programs/hyprland/variables/hyprappearance.nix
+++ home/quinno/programs/hyprland/variables/hyprappearance.nix
@@ -1,76 +1,78 @@
 {
-  decoration = {
-    rounding = 6;
-    active_opacity = 1.0;
-    inactive_opacity = 1.0;
-    fullscreen_opacity = 1.0;
-    dim_inactive = false;
-    dim_strength = 0.5;
-    dim_special = 0.2;
-    dim_around = 0.4;
-    # screen_shader = ;
+  wayland.windowManager.hyprland.settings = {
+    decoration = {
+      rounding = 6;
+      active_opacity = 1.0;
+      inactive_opacity = 1.0;
+      fullscreen_opacity = 1.0;
+      dim_inactive = false;
+      dim_strength = 0.5;
+      dim_special = 0.2;
+      dim_around = 0.4;
+      # screen_shader = ;
 
-    blur = {
-      enabled = true;
-      size = 8;
-      ignore_opacity = true;
-      new_optimizations = true;
-      xray = false;
-      noise = 0.117;
-      contrast = 0.8916;
-      brightness = 0.8172;
-      vibrancy = 0.1696;
-      vibrancy_darkness = 0.0;
-      special = false;
-      popups = false;
-      popups_ignorealpha = 0.2;
+      blur = {
+        enabled = true;
+        size = 8;
+        ignore_opacity = true;
+        new_optimizations = true;
+        xray = false;
+        noise = 0.117;
+        contrast = 0.8916;
+        brightness = 0.8172;
+        vibrancy = 0.1696;
+        vibrancy_darkness = 0.0;
+        special = false;
+        popups = false;
+        popups_ignorealpha = 0.2;
+      };
+
+      shadow = {
+        enabled = true;
+        range = 6;
+        render_power = 1;
+        sharp = false;
+        ignore_window = true;
+        color = "rgba(0000004d)";
+        color_inactive = "rgba(00000040)";
+        offset = "0, 0";
+        scale = 2.0;
+      };
     };
 
-    shadow = {
+    animations = {
       enabled = true;
-      range = 6;
-      render_power = 1;
-      sharp = false;
-      ignore_window = true;
-      color = "rgba(0000004d)";
-      color_inactive = "rgba(00000040)";
-      offset = "0, 0";
-      scale = 2.0;
+      first_launch_animation = true;
     };
-  };
+    bezier = [
+      "linear, 0, 0, 1, 1"
+      "md3_standard, 0.2, 0, 0, 1"
+      "md3_decel, 0.05, 0.7, 0.1, 1"
+      "md3_accel, 0.3, 0, 0.8, 0.15"
+      "overshot, 0.05, 0.9, 0.1, 1.1"
+      "crazyshot, 0.1, 1.5, 0.76, 0.92"
+      "hyprnostretch, 0.05, 0.9, 0.1, 1.0"
+      "menu_decel, 0.1, 1, 0, 1"
+      "menu_accel, 0.38, 0.04, 1, 0.07"
+      "easeInOutCirc, 0.85, 0, 0.15, 1"
+      "easeOutCirc, 0, 0.55, 0.45, 1"
+      "easeOutExpo, 0.16, 1, 0.3, 1"
+      "softAcDecel, 0.26, 0.26, 0.15, 1"
+      "md2, 0.4, 0, 0.2, 1" # use with .2s duration
+    ];
 
-  animations = {
-    enabled = true;
-    first_launch_animation = true;
+    animation = [
+      "windows, 1, 3, md3_decel, popin 60%"
+      "windowsIn, 1, 3, md3_decel, popin 60%"
+      "windowsOut, 1, 3, md3_accel, popin 60%"
+      "border, 1, 10, default"
+      "fade, 1, 3, md3_decel"
+      "layersIn, 1, 3, menu_decel, slide"
+      "layersOut, 1, 1.6, menu_accel"
+      "fadeLayersIn, 1, 2, menu_decel"
+      "fadeLayersOut, 1, 4.5, menu_accel"
+      "workspaces, 1, 7, menu_decel, slide"
+      "specialWorkspace, 1, 3, md3_decel, slidevert"
+    ];
   };
-  bezier = [
-    "linear, 0, 0, 1, 1"
-    "md3_standard, 0.2, 0, 0, 1"
-    "md3_decel, 0.05, 0.7, 0.1, 1"
-    "md3_accel, 0.3, 0, 0.8, 0.15"
-    "overshot, 0.05, 0.9, 0.1, 1.1"
-    "crazyshot, 0.1, 1.5, 0.76, 0.92"
-    "hyprnostretch, 0.05, 0.9, 0.1, 1.0"
-    "menu_decel, 0.1, 1, 0, 1"
-    "menu_accel, 0.38, 0.04, 1, 0.07"
-    "easeInOutCirc, 0.85, 0, 0.15, 1"
-    "easeOutCirc, 0, 0.55, 0.45, 1"
-    "easeOutExpo, 0.16, 1, 0.3, 1"
-    "softAcDecel, 0.26, 0.26, 0.15, 1"
-    "md2, 0.4, 0, 0.2, 1" # use with .2s duration
-  ];
-
-  animation = [
-    "windows, 1, 3, md3_decel, popin 60%"
-    "windowsIn, 1, 3, md3_decel, popin 60%"
-    "windowsOut, 1, 3, md3_accel, popin 60%"
-    "border, 1, 10, default"
-    "fade, 1, 3, md3_decel"
-    "layersIn, 1, 3, menu_decel, slide"
-    "layersOut, 1, 1.6, menu_accel"
-    "fadeLayersIn, 1, 2, menu_decel"
-    "fadeLayersOut, 1, 4.5, menu_accel"
-    "workspaces, 1, 7, menu_decel, slide"
-    "specialWorkspace, 1, 3, md3_decel, slidevert"
-  ];
 }
--- home/quinno/programs/hyprland/variables/hyprmisc.nix
+++ home/quinno/programs/hyprland/variables/hyprmisc.nix
@@ -1,56 +1,58 @@
 {
-  misc = {
-    disable_hyprland_logo = true;
-    disable_splash_rendering = true;
-    col.splash = "rgb($onBackground)";
-    font_family = "$font";
-    # splash_font_family=;
-    force_default_wallpaper = 0;
-    vfr = true; # Heavily recommended to leave enabled to conserve resources.
-    vrr = 2;
-    mouse_move_enables_dpms = false;
-    key_press_enables_dpms = true;
-    always_follow_on_dnd = true;
-    layers_hog_keyboard_focus = false;
-    animate_manual_resizes = false;
-    animate_mouse_windowdragging = true;
-    disable_autoreload = false;
-    enable_swallow = true;
-    swallow_regex = foot;
-    # swallow_exception_regex=;
-    focus_on_activate = false;
-    mouse_move_focuses_monitor = true;
-    render_ahead_of_time = false; # [Warning: buggy] starts rendering before your monitor displays a frame in order to lower latency
-    render_ahead_safezone = 1; # how many ms of safezone to add to rendering ahead of time. Recommended 1-2.
-    allow_session_lock_restore = false;
-    background_color = "rgb($background)";
-    close_special_on_empty = true;
-    new_window_takes_over_fullscreen = 2;
-    exit_window_retains_fullscreen = false;
-    initial_workspace_tracking = 1;
-    middle_click_paste = false;
-    render_unfocused_fps = 15;
-    disable_xdg_env_checks = false;
-  };
+  wayland.windowManager.hyprland.settings = {
+    misc = {
+      disable_hyprland_logo = true;
+      disable_splash_rendering = true;
+      col.splash = "rgb($onBackground)";
+      font_family = "$font";
+      # splash_font_family=;
+      force_default_wallpaper = 0;
+      vfr = true; # Heavily recommended to leave enabled to conserve resources.
+      vrr = 2;
+      mouse_move_enables_dpms = false;
+      key_press_enables_dpms = true;
+      always_follow_on_dnd = true;
+      layers_hog_keyboard_focus = false;
+      animate_manual_resizes = false;
+      animate_mouse_windowdragging = true;
+      disable_autoreload = false;
+      enable_swallow = true;
+      swallow_regex = foot;
+      # swallow_exception_regex=;
+      focus_on_activate = false;
+      mouse_move_focuses_monitor = true;
+      render_ahead_of_time = false; # [Warning: buggy] starts rendering before your monitor displays a frame in order to lower latency
+      render_ahead_safezone = 1; # how many ms of safezone to add to rendering ahead of time. Recommended 1-2.
+      allow_session_lock_restore = false;
+      background_color = "rgb($background)";
+      close_special_on_empty = true;
+      new_window_takes_over_fullscreen = 2;
+      exit_window_retains_fullscreen = false;
+      initial_workspace_tracking = 1;
+      middle_click_paste = false;
+      render_unfocused_fps = 15;
+      disable_xdg_env_checks = false;
+    };
 
-  xwayland = {
-    enabled = true;
-    use_nearest_neighbor = true;
-    force_zero_scaling = false;
-  };
+    xwayland = {
+      enabled = true;
+      use_nearest_neighbor = true;
+      force_zero_scaling = false;
+    };
 
-  opengl = {
-    nvidia_anti_flicker = true; # On non-nvidia, this is ignored.
-    force_introspection = 1; # Introspection is aimed at reducing GPU usage in certain cases, but might cause graphical glitches on nvidia.
-  };
+    opengl = {
+      nvidia_anti_flicker = true; # On non-nvidia, this is ignored.
+      force_introspection = 1; # Introspection is aimed at reducing GPU usage in certain cases, but might cause graphical glitches on nvidia.
+    };
 
-  render = {
-    explicit_sync = 2;
-    explicit_sync_kms = 2;
-    direct_scanout = true; # Enables direct scanout. Direct scanout attempts to reduce lag when there is only one fullscreen application on a screen (e.g. game). It is also recommended to set this to false if the fullscreen application shows graphical glitches.
-  };
+    render = {
+      explicit_sync = 2;
+      explicit_sync_kms = 2;
+      direct_scanout = true; # Enables direct scanout. Direct scanout attempts to reduce lag when there is only one fullscreen application on a screen (e.g. game). It is also recommended to set this to false if the fullscreen application shows graphical glitches.
+    };
 
-  debug = {
-    enable_stdout_logs = false;
+    debug = {
+      enable_stdout_logs = false;
+    };
   };
 }
