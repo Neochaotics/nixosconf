e04350fdc0a3ac5bdb57453a9100aa4ad1bf8362
Neochaotics <neochaotics@pm.me>
2025-01-01 14:47:40 -0500

    unneeded

--- common/hyprland.nix
+++ common/hyprland.nix
@@ -4,13 +4,6 @@
 }: {
   programs.uwsm = {
     enable = true;
-    waylandCompositors = {
-      hyprland = {
-        prettyName = "Hyprland";
-        comment = "Hyprland compositor managed by UWSM";
-        binPath = "/run/current-system/sw/bin/Hyprland";
-      };
-    };
   };
   programs.hyprland = {
     enable = true;
