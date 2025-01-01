c94889286724b74f2d4c0c4a35b7fd38072ca63f
Neochaotics <neochaotics@pm.me>
2025-01-01 14:40:54 -0500

    fix uwsm?

--- home/quinno/programs/hyprland/default.nix
+++ home/quinno/programs/hyprland/default.nix
@@ -21,21 +21,4 @@
     xwayland.enable = true;
     extraConfig = builtins.readFile ./hyprconf.conf;
   };
-  # https://github.com/hyprwm/hyprland-wiki/issues/409
-  # https://github.com/nix-community/home-manager/pull/4707
-  xdg.portal = {
-    config = {
-      common = {
-        default = [ "hyprland" ];
-        #"org.freedesktop.impl.portal.Secret" = [ "gnome-keyring" ];
-      };
-    };
-    configPackages = [ config.wayland.windowManager.hyprland.package ];
-    enable = true;
-    extraPortals = [
-      pkgs.xdg-desktop-portal
-      pkgs.xdg-desktop-portal-hyprland
-    ];
-    xdgOpenUsePortal = true;
-  };
 }
