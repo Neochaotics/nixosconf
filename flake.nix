64690518ae8dc1b3713ab2cc7d4f1d014c9bd2d3
Neochaotics <neochaotics@pm.me>
2025-01-02 17:56:50 -0500

    feat: firefox & chrome confs

--- flake.nix
+++ flake.nix
@@ -6,8 +6,6 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-24.11";
 
-    nur.url = "github:nix-community/NUR";
-
     # Home manager
     home-manager.url = "github:nix-community/home-manager";
 
