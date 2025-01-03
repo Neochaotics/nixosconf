e4b6969d02127891d4b5dc3cca5d825a452d4e26
Neochaotics <neochaotics@pm.me>
2025-01-03 15:59:44 -0500

    fix: semicol

--- home/quinno/programs/browsers.nix
+++ home/quinno/programs/browsers.nix
@@ -55,106 +55,106 @@ in
         "DNSOverHTTPS" = {
           "Enabled" = true;
           "ProviderURL" = "https=//dns.quad9.net/dns-query";
-          "Locked" = true
-            };
-          "DontCheckDefaultBrowser" = true;
-          "EnableTrackingProtection" = {
-            "Value" = true;
+          "Locked" = true;
+        };
+        "DontCheckDefaultBrowser" = true;
+        "EnableTrackingProtection" = {
+          "Value" = true;
+          "Locked" = true;
+          "Cryptomining" = true;
+          "Fingerprinting" = true;
+          "EmailTracking" = true;
+        };
+        "EncryptedMediaExtensions" = {
+          "Enabled" = true;
+          "Locked" = true;
+        };
+        "ExtensionUpdate" = true;
+        "FirefoxHome" = {
+          "Search" = false;
+          "TopSites" = false;
+          "SponsoredTopSites" = false;
+          "Highlights" = false;
+          "Pocket" = false;
+          "SponsoredPocket" = false;
+          "Snippets" = false;
+          "Locked" = true;
+        };
+        "FirefoxSuggest" = {
+          "WebSuggestions" = false;
+          "SponsoredSuggestions" = false;
+          "ImproveSuggest" = false;
+          "Locked" = true;
+        };
+        "HardwareAcceleration" = true;
+        "Homepage" = {
+          "Locked" = true;
+          "StartPage" = "none";
+        };
+        "HttpsOnlyMode" = "force_enabled";
+        "InstallAddonsPermission" = {
+          "Default" = false;
+        };
+        "NetworkPrediction" = false;
+        "NewTabPage" = true;
+        "NoDefaultBookmarks" = true;
+        "OfferToSaveLogins" = false;
+        "OverrideFirstRunPage" = "";
+        "OverridePostUpdatePage" = "";
+        "PasswordManagerEnabled" = false;
+        "PopupBlocking" = {
+          "Default" = true;
+        };
+        "Permissions" = {
+          "Camera" = {
+            "BlockNewRequests" = false;
             "Locked" = true;
-            "Cryptomining" = true;
-            "Fingerprinting" = true;
-            "EmailTracking" = true
-              };
-            "EncryptedMediaExtensions" = {
-              "Enabled" = true;
-              "Locked" = true;
+          };
+          "Microphone" = {
+            "BlockNewRequests" = false;
+            "Locked" = true;
+          };
+          "Location" = {
+            "BlockNewRequests" = true;
+            "Locked" = true;
+          };
+          "Notifications" = {
+            "BlockNewRequests" = true;
+            "Locked" = true;
+          };
+          "Autoplay" = {
+            "Default" = "block-audio-video";
+            "Locked" = true;
+          }
             };
-            "ExtensionUpdate" = true;
-            "FirefoxHome" = {
-              "Search" = false;
-              "TopSites" = false;
-              "SponsoredTopSites" = false;
-              "Highlights" = false;
-              "Pocket" = false;
-              "SponsoredPocket" = false;
-              "Snippets" = false;
-              "Locked" = true
-                };
-              "FirefoxSuggest" = {
-                "WebSuggestions" = false;
-                "SponsoredSuggestions" = false;
-                "ImproveSuggest" = false;
-                "Locked" = true;
-              };
-              "HardwareAcceleration" = true;
-              "Homepage" = {
-                "Locked" = true;
-                "StartPage" = "none";
-              };
-              "HttpsOnlyMode" = "force_enabled";
-              "InstallAddonsPermission" = {
-                "Default" = false
-                  };
-                "NetworkPrediction" = false;
-                "NewTabPage" = true;
-                "NoDefaultBookmarks" = true;
-                "OfferToSaveLogins" = false;
-                "OverrideFirstRunPage" = "";
-                "OverridePostUpdatePage" = "";
-                "PasswordManagerEnabled" = false;
-                "PopupBlocking" = {
-                  "Default" = true;
-                };
-                "Permissions" = {
-                  "Camera" = {
-                    "BlockNewRequests" = false;
-                    "Locked" = true
-                      };
-                    "Microphone" = {
-                      "BlockNewRequests" = false;
-                      "Locked" = true
-                        };
-                      "Location" = {
-                        "BlockNewRequests" = true;
-                        "Locked" = true
-                          };
-                        "Notifications" = {
-                          "BlockNewRequests" = true;
-                          "Locked" = true
-                            };
-                          "Autoplay" = {
-                            "Default" = "block-audio-video";
-                            "Locked" = true
-                              }
-                              };
-                            "PictureInPicture" = {
-                              "Enabled" = false;
-                              "Locked" = true
-                                };
-                              "PostQuantumKeyAgreementEnabled" = true;
-                              "SearchBar" = "unified";
-                              "SearchSuggestEnabled" = false;
-                              "ShowHomeButton" = false;
-                              "StartDownloadsInTempDirectory" = true;
-                              "SanitizeOnShutdown" = {
-                                "Cache" = true;
-                                "Cookies" = true;
-                                "History" = true;
-                                "Sessions" = true;
-                                "SiteSettings" = true;
-                                "Locked" = true
-                                  };
-                                "UserMessaging" = {
-                                  "WhatsNew" = false;
-                                  "ExtensionRecommendations" = true;
-                                  "FeatureRecommendations" = false;
-                                  "UrlbarInterventions" = false;
-                                  "SkipOnboarding" = true;
-                                  "MoreFromMozilla" = false;
-                                  "Locked" = true;
-                                };
-                                "UseSystemPrintDialog" = true;
-                              };
-                            };
-                          };
-                        }
+          "PictureInPicture" = {
+            "Enabled" = false;
+            "Locked" = true;
+          };
+          "PostQuantumKeyAgreementEnabled" = true;
+          "SearchBar" = "unified";
+          "SearchSuggestEnabled" = false;
+          "ShowHomeButton" = false;
+          "StartDownloadsInTempDirectory" = true;
+          "SanitizeOnShutdown" = {
+            "Cache" = true;
+            "Cookies" = true;
+            "History" = true;
+            "Sessions" = true;
+            "SiteSettings" = true;
+            "Locked" = true;
+          };
+          "UserMessaging" = {
+            "WhatsNew" = false;
+            "ExtensionRecommendations" = true;
+            "FeatureRecommendations" = false;
+            "UrlbarInterventions" = false;
+            "SkipOnboarding" = true;
+            "MoreFromMozilla" = false;
+            "Locked" = true;
+          };
+          "UseSystemPrintDialog" = true;
+        };
+      };
+    };
+  }
