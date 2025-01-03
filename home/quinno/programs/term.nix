0d12755123a70506f3c17c0abcc8eb75f7fc33f1
Neochaotics <neochaotics@pm.me>
2025-01-02 20:15:58 -0500

    fix: forgot main

--- home/quinno/programs/term.nix
+++ home/quinno/programs/term.nix
@@ -2,10 +2,12 @@
   programs.foot = {
     enable = true;
     settings = {
-      term = "xterm-256color";
+      main = {
+        term = "xterm-256color";
 
-      font = "Fira Code:size=11";
-      dpi-aware = "yes";
+        font = "Fira Code:size=11";
+        dpi-aware = "yes";
+      };
       mouse = {
         hide-when-typing = "yes";
       };
