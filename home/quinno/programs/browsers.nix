6faad6bde443a545727193027263204a080b8cc7
Neochaotics <neochaotics@pm.me>
2025-01-03 18:39:22 -0500

    test: firefox

--- home/quinno/programs/browsers.nix
+++ home/quinno/programs/browsers.nix
@@ -153,6 +153,208 @@ in
           "MoreFromMozilla" = false;
           "Locked" = true;
         };
+        "Preferences" = {
+          "browser.aboutConfig.showWarning" = {
+            Value = false;
+            Status = "locked";
+          };
+          "browser.newtab.preload" = {
+            Value = false;
+            Status = "locked";
+          };
+          "browser.newtabpage.activity-stream.feeds.telemetry" = {
+            Value = false;
+            Status = "locked";
+          };
+          "geo.provider.use_geoclue" = {
+            Value = false;
+            Status = "locked";
+          };
+          "browser.region.update.enabled" = {
+            Value = false;
+            Status = "locked";
+          };
+          "app.update.auto" = {
+            Value = false;
+            Status = "locked";
+          };
+          "browser.discovery.enabled" = {
+            Value = false;
+            Status = "locked";
+          };
+          "extensions.htmlaboutaddons.recommendations.enabled" = {
+            Value = false;
+            Status = "locked";
+          };
+          "network.connectivity-service.enabled" = {
+            Value = false;
+            Status = "locked";
+          };
+          "browser.safebrowsing.malware.enabled" = {
+            Value = false;
+            Status = "locked";
+          };
+          "browser.safebrowsing.phishing.enabled" = {
+            Value = false;
+            Status = "locked";
+          };
+          "browser.tabs.crashReporting.sendReport" = {
+            Value = false;
+            Status = "locked";
+          };
+          "browser.cache.disk.enable" = {
+            Value = false;
+            Status = "locked";
+          };
+          "browser.safebrowsing.provider.google4.gethashURL" = {
+            Value = "";
+            Status = "locked";
+          };
+          "browser.safebrowsing.provider.google4.updateURL" = {
+            Value = "";
+            Status = "locked";
+          };
+          "browser.safebrowsing.provider.google.gethashURL" = {
+            Value = "";
+            Status = "locked";
+          };
+          "browser.safebrowsing.provider.google.updateURL" = {
+            Value = "";
+            Status = "locked";
+          };
+          "browser.safebrowsing.provider.google4.dataSharingURL" = {
+            Value = "";
+            Status = "locked";
+          };
+          "browser.safebrowsing.downloads.enabled" = {
+            Value = false;
+            Status = "locked";
+          };
+          "browser.safebrowsing.downloads.remote.enabled" = {
+            Value = false;
+            Status = "locked";
+          };
+          "browser.safebrowsing.downloads.remote.url" = {
+            Value = "";
+            Status = "locked";
+          };
+          "browser.safebrowsing.downloads.remote.block_potentially_unwanted" = {
+            Value = false;
+            Status = "locked";
+          };
+          "browser.safebrowsing.downloads.remote.block_uncommon" = {
+            Value = false;
+            Status = "locked";
+          };
+          "network.prefetch-next" = {
+            Value = false;
+            Status = "locked";
+          };
+          "network.dns.disablePrefetch" = {
+            Value = false;
+            Status = "locked";
+          };
+          "network.predictor.enabled" = {
+            Value = false;
+            Status = "locked";
+          };
+          "network.http.speculative-parallel-limit" = {
+            Value = 0;
+            Status = "locked";
+          };
+          "browser.places.speculativeConnect.enabled" = {
+            Value = false;
+            Status = "locked";
+          };
+          "network.dns.disableIPv6" = {
+            Value = true;
+            Status = "locked";
+          };
+          "browser.urlbar.trimURLs" = {
+            Value = false;
+            Status = "locked";
+          };
+          "browser.urlbar.speculativeConnect.enabled" = {
+            Value = false;
+            Status = "locked";
+          };
+          "browser.sessionstore.privacy_level" = {
+            Value = 2;
+            Status = "locked";
+          };
+          "browser.sessionstore.resume_from_crash" = {
+            Value = false;
+            Status = "locked";
+          };
+          "browser.shell.shortcutFavicons" = {
+            Value = false;
+            Status = "locked";
+          };
+          "dom.security.https_only_mode_send_http_background_request" = {
+            Value = false;
+            Status = "locked";
+          };
+          "browser.xul.error_pages.expert_bad_cert" = {
+            Value = true;
+            Status = "locked";
+          };
+          "network.http.referer.XOriginPolicy" = {
+            Value = 2;
+            Status = "locked";
+          };
+          "network.http.referer.XOriginTrimmingPolicy" = {
+            Value = 2;
+            Status = "locked";
+          };
+          "media.peerconnection.ice.proxy_only_if_behind_proxy" = {
+            Value = true;
+            Status = "locked";
+          };
+          "media.peerconnection.ice.default_address_only" = {
+            Value = true;
+            Status = "locked";
+          };
+          "media.peerconnection.ice.no_host" = {
+            Value = true;
+            Status = "locked";
+          };
+          "media.eme.enabled" = {
+            Value = false;
+            Status = "locked";
+          };
+          "browser.contentblocking.category" = {
+            Value = "strict";
+            Status = "locked";
+          };
+          "privacy.resistFingerprinting" = {
+            Value = true;
+            Status = "locked";
+          };
+          "privacy.window.maxInnerWidth" = {
+            Value = 1600;
+            Status = "locked";
+          };
+          "privacy.window.maxInnerHeight" = {
+            Value = 900;
+            Status = "locked";
+          };
+          "browser.display.use_system_colors" = {
+            Value = true;
+            Status = "locked";
+          };
+          "browser.safebrowsing.allowOverride" = {
+            Value = false;
+            Status = "locked";
+          };
+          "browser.safebrowsing.allowOverride" = {
+            Value = false;
+            Status = "locked";
+          };
+          "browser.safebrowsing.allowOverride" = {
+            Value = false;
+            Status = "locked";
+          };
+        };
         "UseSystemPrintDialog" = true;
       };
     };
