40cc44c5b16afda7579c4ac56d9dc6ed5f9d8153
Neochaotics <neochaotics@pm.me>
2025-01-10 19:45:38 -0500

    feat: trim

--- modules/home/quinno/programs/common.nix
+++ modules/home/quinno/programs/common.nix
@@ -12,9 +12,6 @@
 
     # utils
     ripgrep # recursively searches directories for a regex pattern
-    #jq # A lightweight and flexible command-line JSON processor
-    yq-go # yaml processor https://github.com/mikefarah/yq
-    #eza # A modern replacement for ‘ls’
     fzf # A command-line fuzzy finder
 
     # nix related
@@ -41,49 +38,4 @@
 
     nil
   ];
-
-  programs = {
-    starship = {
-      enable = true;
-      # custom settings
-      settings = {
-        add_newline = false;
-        aws.disabled = true;
-        gcloud.disabled = true;
-        line_break.disabled = true;
-      };
-    };
-    alacritty = {
-      enable = true;
-      # custom settings
-      settings = {
-        env.TERM = "xterm-256color";
-        font = {
-          size = 12;
-          draw_bold_text_with_bright_colors = true;
-        };
-        scrolling.multiplier = 5;
-        selection.save_to_clipboard = true;
-      };
-    };
-    bash = {
-      enable = true;
-      enableCompletion = true;
-      # TODO add your custom bashrc here
-      bashrcExtra = ''
-        export PATH="$PATH:$HOME/bin:$HOME/.local/bin:$HOME/go/bin"
-      '';
-    };
-    btop.enable = true; # replacement of htop/nmon
-    eza.enable = true; # A modern replacement for ‘ls’
-    jq.enable = true; # A lightweight and flexible command-line JSON processor
-    ssh.enable = true;
-  };
-
-  services = {
-    syncthing.enable = true;
-
-    # auto mount usb drives
-    udiskie.enable = true;
-  };
 }
