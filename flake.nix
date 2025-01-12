710537a8fe69dfc8e46d222d79bfa3dcc1e55115
Neochaotics <neochaotics@pm.me>
2025-01-11 19:42:42 -0500

    feat: nix develop, fmt, lint, & lsp

--- flake.nix
+++ flake.nix
@@ -18,47 +18,47 @@
     impermanence.url = "github:nix-community/impermanence";
   };
 
-  outputs = {
-    self,
-    nixpkgs,
-    nixpkgs-stable,
-    home-manager,
-    ...
-  } @ inputs: let
-    inherit (self) outputs;
-    system = "x86_64-linux";
+  outputs =
+    {
+      self,
+      nixpkgs,
+      nixpkgs-stable,
+      home-manager,
+      ...
+    }@inputs:
+    let
+      inherit (self) outputs;
+      system = "x86_64-linux";
 
-    pkgs = import nixpkgs {
-      inherit system;
-    };
+      pkgs = import nixpkgs { inherit system; };
 
-    # Load all hosts from the hosts directory
-    hostNames = builtins.attrNames (builtins.readDir ./hosts);
-    hostConfigs = nixpkgs.lib.genAttrs hostNames (hostname:
-      nixpkgs.lib.nixosSystem {
-        specialArgs = {
-          inherit inputs outputs hostname;
-          pkgs-stable = import nixpkgs-stable {
-            inherit system;
+      # Load all hosts from the hosts directory
+      hostNames = builtins.attrNames (builtins.readDir ./hosts);
+      hostConfigs = nixpkgs.lib.genAttrs hostNames (
+        hostname:
+        nixpkgs.lib.nixosSystem {
+          specialArgs = {
+            inherit inputs outputs hostname;
+            pkgs-stable = import nixpkgs-stable { inherit system; };
           };
-        };
-        modules = [ ./hosts/${hostname} ];
-      }
-    );
-  in {
-    # NixOS configurations
-    nixosConfigurations = hostConfigs;
+          modules = [ ./hosts/${hostname} ];
+        }
+      );
+    in
+    {
+      # NixOS configurations
+      nixosConfigurations = hostConfigs;
 
-    # Development shell
-    devShells.${system}.default = pkgs.mkShell {
-      packages = with pkgs; [
-        nil
-        nixfmt
-        statix
-      ];
+      # Development shell
+      devShells.${system}.default = pkgs.mkShell {
+        packages = with pkgs; [
+          nil
+          nixfmt-rfc-style
+          statix
+        ];
+      };
+
+      # Formatter configuration
+      formatter.${system} = pkgs.nixfmt-rfc-style;
     };
-
-    # Formatter configuration
-    formatter.${system} = pkgs.nixfmt;
-  };
 }
