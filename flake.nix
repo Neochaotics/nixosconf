b34f074253f48e98508140aded5660a45bcd3a48
Neochaotics <neochaotics@pm.me>
2025-01-11 19:08:59 -0500

    test: devShells and new hostConfig function

--- flake.nix
+++ flake.nix
@@ -1,41 +1,64 @@
 {
-  description = "NIXOS";
+  description = "NixOS System Configuration";
 
   inputs = {
-    # Nixpkgs
+    # Core dependencies
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-24.11";
 
-    # Home manager
-    home-manager.url = "github:nix-community/home-manager";
-
-    disko.url = "github:nix-community/disko/latest";
-
+    # System management
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    disko = {
+      url = "github:nix-community/disko/latest";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     impermanence.url = "github:nix-community/impermanence";
   };
 
-  outputs =
-    {
-      self,
-      nixpkgs,
-      nixpkgs-stable,
-      home-manager,
-      ...
-    }@inputs:
-    let
-      inherit (self) outputs;
-      hostConfigs = nixpkgs.lib.foldl (
-        acc: host:
-        let
-          systemConfig = nixpkgs.lib.nixosSystem {
-            specialArgs = { inherit inputs outputs; };
-            modules = [ ./hosts/${host} ];
-          };
-        in
-        acc // { ${host} = systemConfig; }
-      ) { } (nixpkgs.lib.attrNames (builtins.readDir ./hosts));
-    in
-    {
-      nixosConfigurations = hostConfigs;
+  outputs = {
+    self,
+    nixpkgs,
+    nixpkgs-stable,
+    home-manager,
+    ...
+  } @ inputs: let
+    inherit (self) outputs;
+    system = "x86_64-linux";
+
+    pkgs = import nixpkgs {
+      inherit system;
     };
+
+    # Load all hosts from the hosts directory
+    hostNames = builtins.attrNames (builtins.readDir ./hosts);
+    hostConfigs = nixpkgs.lib.genAttrs hostNames (hostname:
+      nixpkgs.lib.nixosSystem {
+        specialArgs = {
+          inherit inputs outputs hostname;
+          pkgs-stable = import nixpkgs-stable {
+            inherit system;
+          };
+        };
+        modules = [ ./hosts/${hostname} ];
+      }
+    );
+  in {
+    # NixOS configurations
+    nixosConfigurations = hostConfigs;
+
+    # Development shell
+    devShells.${system}.default = pkgs.mkShell {
+      packages = with pkgs; [
+        nil
+        nixfmt
+        statix
+      ];
+    };
+
+    # Formatter configuration
+    formatter.${system} = pkgs.nixfmt;
+  };
 }
