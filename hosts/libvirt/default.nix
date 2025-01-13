d986a07c81816537e879c2725e7c4b5d31575fea
Neochaotics <neochaotics@pm.me>
2025-01-13 17:56:48 -0500

    test: trim

--- hosts/libvirt/default.nix
+++ hosts/libvirt/default.nix
@@ -46,19 +46,6 @@
     variant = "";
   };
 
-  # Enable CUPS to print documents.
-  services.printing.enable = true;
-
-  # Enable sound with pipewire.
-  hardware.pulseaudio.enable = false;
-  security.rtkit.enable = true;
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-  };
-
   time.timeZone = "America/New_York";
 
   i18n.defaultLocale = "en_US.UTF-8";
