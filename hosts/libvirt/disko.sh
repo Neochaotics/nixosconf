b1853916b883a1dd1ddd6ba90e3291028d968401
Neochaotics <neochaotics@pm.me>
2025-01-25 18:23:13 -0500

    fix: disko

--- hosts/libvirt/disko.sh
+++ hosts/libvirt/disko.sh
@@ -1,1 +1,2 @@
-sudo nix run 'github:nix-community/disko/latest#disko-install' --extra-experimental-features 'nix-command flakes' -- --flake ~/megaflake#libvirt --disk primary /dev/disk/by-id/virtio-CT1000P5PSSD8_22343AC3C7AD --disk secondary /dev/disk/by-id/virtio-CT2000P5PSSD8_23113F244794
+#sudo nix run 'github:nix-community/disko/latest#disko-install' --extra-experimental-features 'nix-command flakes' -- --flake ~/megaflake#libvirt --disk primary /dev/disk/by-id/virtio-CT1000P5PSSD8_22343AC3C7AD --disk secondary /dev/disk/by-id/virtio-CT2000P5PSSD8_23113F244794
+sudo nix --experimental-features "nix-command flakes" run github:nix-community/disko/latest -- --mode destroy,format,mount ~/megaflake/hosts/libvirt/disko.nix
